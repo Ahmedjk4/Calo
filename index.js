cb86df810281e72e6598663ee07e3ce46f706044
Ahmed <70341756+Ahmedjk4@users.noreply.github.com>
2023-05-13 02:10:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports.cc = require('./data/functions/calo');
+module.exports.ccold = require('./data/functions/calo(OLD)')
+module.exports.rand = require('./data/functions/rand');
+module.exports.random = require('./data/functions/random')
+module.exports.time = require('./data/functions/time');
+
