c96cd1ca50be962ec6aec055258784acaba0dee1
Ahmed <70341756+Ahmedjk4@users.noreply.github.com>
2023-05-14 02:56:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports.cc = require('./data/functions/calo');
+module.exports.ccold = require('./data/functions/calo(OLD)')
+module.exports.rand = require('./data/functions/rand');
+module.exports.random = require('./data/functions/random')
+module.exports.time = require('./data/functions/time');
+module.exports.convert = require('./data/functions/money');
+module.exports.updateRates = require('./data/functions/money').update;
+module.exports.power = require('./data/functions/power');
+module.exports.volume = require('./data/functions/volume')
+/* Copyright (c) 2023 AMEIA Ltd <support@ahmedmohamedelessawy.tk>
+
+Permission to use, copy, modify, and/or distribute this software for
+any purpose with or without fee is hereby granted, provided that the
+above copyright notice and this permission notice appear in all
+copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+*/
